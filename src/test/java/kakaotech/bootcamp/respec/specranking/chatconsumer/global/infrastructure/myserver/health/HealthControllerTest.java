8fd3e66f8bc4881673ef656c499ac1b0a1dc52fa
jeli01 <jeli01@naver.com>
2025-06-24 09:53:46 +0900

    refactor: health check 테스트 코드명 명확히 리팩토

--- src/test/java/kakaotech/bootcamp/respec/specranking/chatconsumer/global/infrastructure/myserver/health/HealthControllerTest.java
+++ src/test/java/kakaotech/bootcamp/respec/specranking/chatconsumer/global/infrastructure/myserver/health/HealthControllerTest.java
@@ -3,6 +3,7 @@ package kakaotech.bootcamp.respec.specranking.chatconsumer.global.infrastructure
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,9 +14,10 @@ class HealthControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-    
+
     @Test
-    void health() throws Exception {
+    @DisplayName("GET /api/health 요청 시 OK(200)을 반환한다")
+    void health_check_request_returns_200() throws Exception {
         mockMvc.perform(get("/api/health"))
                 .andExpect(status().isOk());
     }
