20fcc471dddccc2d37cd3fb3f3e19b1cbadc45ac
jeli01 <jeli01@naver.com>
2025-06-24 13:06:27 +0900

    chore: 쓰이지 않는 코드 제거

--- src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/user/exception/UserException.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/user/exception/UserException.java
@@ -5,8 +5,5 @@ public class UserException extends RuntimeException {
     public UserException(String message) {
         super(message);
     }
-
-    public UserException(String message, Throwable cause) {
-        super(message, cause);
-    }
+    
 }
