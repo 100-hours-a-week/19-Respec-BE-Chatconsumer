eee3e369c92b20c48795809ad6653c0aa856e5f1
jeli01 <jeli01@naver.com>
2025-06-25 17:03:17 +0900

    refactor(ChatDeliveryServiceTest): Fixture를 이용해 가독성 향상

---
+++ src/test/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chat/fixture/ChatFixture.java
@@ -0,0 +1,14 @@
+package kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chat.fixture;
+
+import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chat.dto.ChatRelayDto;
+
+public class ChatFixture {
+
+    private static final Long senderId = 1L;
+    private static final Long receiverId = 2L;
+    private static final String CHAT_MESSAGE = "안녕하세요!";
+
+    public static ChatRelayDto create() {
+        return new ChatRelayDto(senderId, receiverId, CHAT_MESSAGE);
+    }
+}
--- src/test/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chat/service/ChatDeliveryServiceTest.java
+++ src/test/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chat/service/ChatDeliveryServiceTest.java
@@ -8,8 +8,10 @@ import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chat.dto.ChatRelayDto;
+import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chat.fixture.ChatFixture;
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.notification.service.NotificationService;
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.user.entity.User;
+import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.user.fixture.UserFixture;
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.user.service.UserServerLocationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -34,23 +36,14 @@ class ChatDeliveryServiceTest {
     @Mock
     NotificationService notificationService;
 
-    private final Long senderId = 1L;
-    private final Long receiverId = 2L;
-    private final String CHAT_MESSAGE = "안녕하세요!";
-    private final String LOGIN_ID = "receiver@example.com";
-    private final String PASSWORD = "receiver-password";
-    private final String PROFILE_IMAGE_URL = "http://example.profile.jpg";
-    private final String NICKNAME = "http://example.profile.jpg";
-    private final Boolean isOpenSpec = true;
-
     private User receiver;
     private ChatRelayDto chatRelayDto;
 
 
     @BeforeEach
     void setUp() {
-        receiver = new User(LOGIN_ID, PASSWORD, PROFILE_IMAGE_URL, NICKNAME, isOpenSpec);
-        chatRelayDto = new ChatRelayDto(senderId, receiverId, CHAT_MESSAGE);
+        receiver = UserFixture.create();
+        chatRelayDto = ChatFixture.create();
     }
 
     @Test
